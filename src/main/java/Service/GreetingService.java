e822d46b10c1b873d0cf584b71896ffacd8e5d1e
pihu2810 <vishaka.kadam01@gmail.com>
2022-05-18 18:21:30 +0530

    Refactor to get Greeting message

--- src/main/java/Service/GreetingService.java
+++ src/main/java/Service/GreetingService.java
@@ -5,6 +5,9 @@ import model.User;
 
 public class GreetingService  implements IGreetingService
 {
+    private static final String template = "Hello, %s!";
+    //private final AtomicLong counter = new AtomicLong();
+
     @Override
     public Greeting addGreeting(User user) {
         String message = String.format(template, (user.toString().isEmpty()) ? "Hello World" : user.toString());
